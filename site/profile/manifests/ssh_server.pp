d4710c1a918ddd25b59fce20b4b2f7b1514b2d71
nickpoida <47073730+nickpoida@users.noreply.github.com>
2019-10-10 15:30:46 +0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4qX6UzeO2B0dc9IMMb4f+ruwUZAQZFnxMdmQ9e6VvoNPEYqa6zbyHICTCOnvou9RPzsONWdq/fxMFgXgz2hJdynJVEPqCgRXPA+c/I0HJ9Sc/nUuXf7tuSOKTA/Xlzdf/DjskUA2RltalnhflddV9yycDEsxpWped1+fvPGdbKrlkni3QbOkGoW322NHBawh/0tBfuspZ7IkVM7fSNtQYWcUK64ZVYMeQtsdjJ1O1UnrZm337xSykDG57KDbx+cKFJkyrgyTdqkrJs2qlRNhOjHtZfaU9c1i1SkksWuWYPvKX92z8rSCDxNYpu37IKJu7IsyAPa8XkIeMVEXTpk1r',
 	}  
 }
